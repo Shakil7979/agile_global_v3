2655d2480b7a374c349a2a6af9d41b647e7d9db8
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2024-10-23 10:04:54 +0600

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -74,6 +74,17 @@ $(document).ready(function(){
  
 
 	// Made Carousel
+	$('.make_carousel').owlCarousel({
+		items:1,
+        margin : 20,
+		loop:true,
+		nav: true,
+		dots:true,
+		autoplay:true,
+		navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+         
+	});
+	// Made Carousel
 	$('.made_carousel').owlCarousel({
 		items:3,
         margin : 20,

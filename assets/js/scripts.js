3f95b08a3fd30d1804c0ac361cf31248015fe096
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2025-03-28 16:02:40 +0600

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -573,12 +573,3 @@ for (let i = 0; i < acc.length; i++) {
 }
  
 
-
-$(document).ready(function() {
-    $(".js-select2").select2();
-    $(".js-select2-multi").select2();
-  
-    $(".large").select2({
-      dropdownCssClass: "big-drop",
-    });
-  });
